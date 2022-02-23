6a872fc5400a0857e38c79483409d5c9a7d937fb
rinatorch <64331817+rinatorch@users.noreply.github.com>
2022-02-23 11:51:54 -0500

    indexing

--- project/expenses/admin.py
+++ project/expenses/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from expenses.models import Summary, Detail
 
-# Register your models here.
+class SummaryAdmin(admin.ModelAdmin):
+    list_display = ("office", "program", "category", "amount")
+    list_filter = ("category", "program")
+    search_fields = ("program",)
+
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ("office", "program", "category", "payee", "purpose", "amount")
+    list_filter = ("category", "program", "purpose")
+    search_fields = ("program", "payee")
+
+admin.site.register(Summary, SummaryAdmin)
+admin.site.register(Detail, DetailAdmin)
--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from expenses.models import Summary, Detail
 
-# Create your views here.
+def index(request):
+  total=Summary.objects.count()
+  return HttpResponse(f"Hello, world. You're at the expenses index and we have {total} summary objects")
